ea8b880ce427c0f04932fad2dc84f0139a1a8ec5
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-14 18:45:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,4 +8,12 @@ class Artist
   def songs
     Song.all.select{|song| song.artist == self}
   end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(song_name)
+    new_song = Song.new(song_name)
+    add_song(new_song)
 end
